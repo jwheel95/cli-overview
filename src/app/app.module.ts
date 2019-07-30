726a74ea429a308b70429528d0c5a88971cb63ac
Joe <joemwheelock@gmail.com>
2019-07-29 18:36:56 -0700

    added component with flags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { DashComponent } from './features/dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule,
